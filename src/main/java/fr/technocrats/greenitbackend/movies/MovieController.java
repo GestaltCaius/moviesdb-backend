a2a1746bc3aaa3199b49c23ac9a7c067a4c9d07c
Rod Guillaume <rodguillaume@tuta.io>
2019-02-04 22:15:17 +0100

    add CrossOrigin annotation in MovieController

--- src/main/java/fr/technocrats/greenitbackend/movies/MovieController.java
+++ src/main/java/fr/technocrats/greenitbackend/movies/MovieController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/movies")
 @Api(value = "movies", description = "Movies operations")
+@CrossOrigin
 public class MovieController {
 
     @Autowired
